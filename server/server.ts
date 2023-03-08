a3685324d6006166ecdf9a7f07da1cda26bb32ac
MelodyDuan <melodyy.duan@gmail.com>
2023-03-08 11:06:53 -0500

    add cookie-parser module

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,23 @@
-const Express = require('express');
-const app = new Express();
+import express, { Express, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+const app: Express = new express();
 const PORT: number = 3000;
 
+const userController = require('./controllers/userController');
+const cookieController = require('./controllers/cookieController');
+const sessionController = require('./controllers/sessionController');
+const postController = require('./controllers/postController');
 
 
+
+/**
+ * parsers for incoming requests:
+ * - json
+ * - urlencoded/form data
+ * - cookies
+*/
+app.use(express.json());
+app.use(express.urlencoded()); 
+app.use(cookieParser());
 
 app.listen(PORT, (console.log('listening on port ', PORT)));
